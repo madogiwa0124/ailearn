4f415d431cfae2ec74944189990bb2f64fd99394
madogiwa0124 <madogiwa0124@gmail.com>
2025-03-08 21:56:07 +0900

    setup dev enviroment + sample project

---
+++ packages/sample/src/index.ts
@@ -0,0 +1,9 @@
+import "./index.css";
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+document.querySelector("#root")!.innerHTML = `
+<div class="content">
+  <h1>Vanilla Rsbuild</h1>
+  <p>Start building amazing things with Rsbuild.</p>
+</div>
+`;
